e4d6cf9bbe7e86afbfedc23ffddb3998748be56c
klaykree <the_sparrow1@hotmail.com>
2018-07-08 17:05:16 +1000

    Inital

---
+++ DrawLine3D.gd
@@ -0,0 +1,97 @@
+extends Node2D
+
+class Line:
+	var Start
+	var End
+	var LineColor
+	var Time
+	
+	func _init(Start, End, LineColor, Time):
+		self.Start = Start
+		self.End = End
+		self.LineColor = LineColor
+		self.Time = Time
+
+var Lines = []
+var RemovedLine = false
+
+func _process(delta):
+	for i in range(len(Lines)):
+		Lines[i].Time -= delta
+	
+	if(len(Lines) > 0 || RemovedLine):
+		update() #Calls _draw
+		RemovedLine = false
+
+func _draw():
+	var Cam = get_viewport().get_camera()
+	for i in range(len(Lines)):
+		var ScreenPointStart = Cam.unproject_position(Lines[i].Start)
+		var ScreenPointEnd = Cam.unproject_position(Lines[i].End)
+		
+		#Dont draw line if either start or end is considered behind the camera
+		#this causes the line to not be drawn sometimes but avoids a bug where the
+		#line is drawn incorrectly
+		if(Cam.is_position_behind(Lines[i].Start) ||
+			Cam.is_position_behind(Lines[i].End)):
+			continue
+		
+		draw_line(ScreenPointStart, ScreenPointEnd, Lines[i].LineColor)
+	
+	#Remove lines that have timed out
+	var i = Lines.size() - 1
+	while (i >= 0):
+		if(Lines[i].Time < 0.0):
+			Lines.remove(i)
+			RemovedLine = true
+		i -= 1
+
+func DrawLine(Start, End, LineColor, Time = 0.0):
+	Lines.append(Line.new(Start, End, LineColor, Time))
+
+func DrawRay(Start, Ray, LineColor, Time = 0.0):
+	Lines.append(Line.new(Start, Start + Ray, LineColor, Time))
+
+func DrawCube(Center, HalfExtents, LineColor, Time = 0.0):
+	#Start at the 'top left'
+	var LinePointStart = Center
+	LinePointStart.x -= HalfExtents
+	LinePointStart.y += HalfExtents
+	LinePointStart.z -= HalfExtents
+	
+	#Draw top square
+	var LinePointEnd = LinePointStart + Vector3(0, 0, HalfExtents * 2.0)
+	DrawLine(LinePointStart, LinePointEnd, LineColor, Time);
+	LinePointStart = LinePointEnd
+	LinePointEnd = LinePointStart + Vector3(HalfExtents * 2.0, 0, 0)
+	DrawLine(LinePointStart, LinePointEnd, LineColor, Time);
+	LinePointStart = LinePointEnd
+	LinePointEnd = LinePointStart + Vector3(0, 0, -HalfExtents * 2.0)
+	DrawLine(LinePointStart, LinePointEnd, LineColor, Time);
+	LinePointStart = LinePointEnd
+	LinePointEnd = LinePointStart + Vector3(-HalfExtents * 2.0, 0, 0)
+	DrawLine(LinePointStart, LinePointEnd, LineColor, Time);
+	
+	#Draw bottom square
+	LinePointStart = LinePointEnd + Vector3(0, -HalfExtents * 2.0, 0)
+	LinePointEnd = LinePointStart + Vector3(0, 0, HalfExtents * 2.0)
+	DrawLine(LinePointStart, LinePointEnd, LineColor, Time);
+	LinePointStart = LinePointEnd
+	LinePointEnd = LinePointStart + Vector3(HalfExtents * 2.0, 0, 0)
+	DrawLine(LinePointStart, LinePointEnd, LineColor, Time);
+	LinePointStart = LinePointEnd
+	LinePointEnd = LinePointStart + Vector3(0, 0, -HalfExtents * 2.0)
+	DrawLine(LinePointStart, LinePointEnd, LineColor, Time);
+	LinePointStart = LinePointEnd
+	LinePointEnd = LinePointStart + Vector3(-HalfExtents * 2.0, 0, 0)
+	DrawLine(LinePointStart, LinePointEnd, LineColor, Time);
+	
+	#Draw vertical lines
+	LinePointStart = LinePointEnd
+	DrawRay(LinePointStart, Vector3(0, HalfExtents * 2.0, 0), LineColor, Time)
+	LinePointStart += Vector3(0, 0, HalfExtents * 2.0)
+	DrawRay(LinePointStart, Vector3(0, HalfExtents * 2.0, 0), LineColor, Time)
+	LinePointStart += Vector3(HalfExtents * 2.0, 0, 0)
+	DrawRay(LinePointStart, Vector3(0, HalfExtents * 2.0, 0), LineColor, Time)
+	LinePointStart += Vector3(0, 0, -HalfExtents * 2.0)
+	DrawRay(LinePointStart, Vector3(0, HalfExtents * 2.0, 0), LineColor, Time)
