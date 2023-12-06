2582bf1ca2808bb85e827feb3667e0c13d702f0c
klaykree <the_sparrow1@hotmail.com>
2023-12-06 20:18:04 +1100

    Update DrawLine3D.gd
    Fix function calls for godot 4

--- DrawLine3D.gd
+++ DrawLine3D.gd
@@ -4,27 +4,27 @@ class Line:
 	var Start
 	var End
 	var LineColor
-	var Time
+	var time
 	
-	func _init(Start, End, LineColor, Time):
+	func _init(Start, End, LineColor, time):
 		self.Start = Start
 		self.End = End
 		self.LineColor = LineColor
-		self.Time = Time
+		self.time = time
 
 var Lines = []
 var RemovedLine = false
 
 func _process(delta):
 	for i in range(len(Lines)):
-		Lines[i].Time -= delta
+		Lines[i].time -= delta
 	
 	if(len(Lines) > 0 || RemovedLine):
-		update() #Calls _draw
+		queue_redraw() #Calls _draw
 		RemovedLine = false
 
 func _draw():
-	var Cam = get_viewport().get_camera()
+	var Cam = get_viewport().get_camera_3d()
 	for i in range(len(Lines)):
 		var ScreenPointStart = Cam.unproject_position(Lines[i].Start)
 		var ScreenPointEnd = Cam.unproject_position(Lines[i].End)
@@ -41,18 +41,18 @@ func _draw():
 	#Remove lines that have timed out
 	var i = Lines.size() - 1
 	while (i >= 0):
-		if(Lines[i].Time < 0.0):
-			Lines.remove(i)
+		if(Lines[i].time < 0.0):
+			Lines.remove_at(i)
 			RemovedLine = true
 		i -= 1
 
-func DrawLine(Start, End, LineColor, Time = 0.0):
-	Lines.append(Line.new(Start, End, LineColor, Time))
+func DrawLine(Start, End, LineColor, time = 0.0):
+	Lines.append(Line.new(Start, End, LineColor, time))
 
-func DrawRay(Start, Ray, LineColor, Time = 0.0):
-	Lines.append(Line.new(Start, Start + Ray, LineColor, Time))
+func DrawRay(Start, Ray, LineColor, time = 0.0):
+	Lines.append(Line.new(Start, Start + Ray, LineColor, time))
 
-func DrawCube(Center, HalfExtents, LineColor, Time = 0.0):
+func DrawCube(Center, HalfExtents, LineColor, time = 0.0):
 	#Start at the 'top left'
 	var LinePointStart = Center
 	LinePointStart.x -= HalfExtents
@@ -61,37 +61,37 @@ func DrawCube(Center, HalfExtents, LineColor, Time = 0.0):
 	
 	#Draw top square
 	var LinePointEnd = LinePointStart + Vector3(0, 0, HalfExtents * 2.0)
-	DrawLine(LinePointStart, LinePointEnd, LineColor, Time);
+	DrawLine(LinePointStart, LinePointEnd, LineColor, time);
 	LinePointStart = LinePointEnd
 	LinePointEnd = LinePointStart + Vector3(HalfExtents * 2.0, 0, 0)
-	DrawLine(LinePointStart, LinePointEnd, LineColor, Time);
+	DrawLine(LinePointStart, LinePointEnd, LineColor, time);
 	LinePointStart = LinePointEnd
 	LinePointEnd = LinePointStart + Vector3(0, 0, -HalfExtents * 2.0)
-	DrawLine(LinePointStart, LinePointEnd, LineColor, Time);
+	DrawLine(LinePointStart, LinePointEnd, LineColor, time);
 	LinePointStart = LinePointEnd
 	LinePointEnd = LinePointStart + Vector3(-HalfExtents * 2.0, 0, 0)
-	DrawLine(LinePointStart, LinePointEnd, LineColor, Time);
+	DrawLine(LinePointStart, LinePointEnd, LineColor, time);
 	
 	#Draw bottom square
 	LinePointStart = LinePointEnd + Vector3(0, -HalfExtents * 2.0, 0)
 	LinePointEnd = LinePointStart + Vector3(0, 0, HalfExtents * 2.0)
-	DrawLine(LinePointStart, LinePointEnd, LineColor, Time);
+	DrawLine(LinePointStart, LinePointEnd, LineColor, time);
 	LinePointStart = LinePointEnd
 	LinePointEnd = LinePointStart + Vector3(HalfExtents * 2.0, 0, 0)
-	DrawLine(LinePointStart, LinePointEnd, LineColor, Time);
+	DrawLine(LinePointStart, LinePointEnd, LineColor, time);
 	LinePointStart = LinePointEnd
 	LinePointEnd = LinePointStart + Vector3(0, 0, -HalfExtents * 2.0)
-	DrawLine(LinePointStart, LinePointEnd, LineColor, Time);
+	DrawLine(LinePointStart, LinePointEnd, LineColor, time);
 	LinePointStart = LinePointEnd
 	LinePointEnd = LinePointStart + Vector3(-HalfExtents * 2.0, 0, 0)
-	DrawLine(LinePointStart, LinePointEnd, LineColor, Time);
+	DrawLine(LinePointStart, LinePointEnd, LineColor, time);
 	
 	#Draw vertical lines
 	LinePointStart = LinePointEnd
-	DrawRay(LinePointStart, Vector3(0, HalfExtents * 2.0, 0), LineColor, Time)
+	DrawRay(LinePointStart, Vector3(0, HalfExtents * 2.0, 0), LineColor, time)
 	LinePointStart += Vector3(0, 0, HalfExtents * 2.0)
-	DrawRay(LinePointStart, Vector3(0, HalfExtents * 2.0, 0), LineColor, Time)
+	DrawRay(LinePointStart, Vector3(0, HalfExtents * 2.0, 0), LineColor, time)
 	LinePointStart += Vector3(HalfExtents * 2.0, 0, 0)
-	DrawRay(LinePointStart, Vector3(0, HalfExtents * 2.0, 0), LineColor, Time)
+	DrawRay(LinePointStart, Vector3(0, HalfExtents * 2.0, 0), LineColor, time)
 	LinePointStart += Vector3(0, 0, -HalfExtents * 2.0)
-	DrawRay(LinePointStart, Vector3(0, HalfExtents * 2.0, 0), LineColor, Time)
+	DrawRay(LinePointStart, Vector3(0, HalfExtents * 2.0, 0), LineColor, time)
